8675b7b4369cc59b5bed95a071208083008d1d3a
Abhinav Sanjeeva Prasad <abhinavsanjeevaprasad@Abhinavs-MacBook-Pro.local>
2022-11-19 12:07:05 -0500

    Finished login required.

--- flaskr-tdd/tests/app_test.py
+++ flaskr-tdd/tests/app_test.py
@@ -76,9 +76,21 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+def test_delete_message_without_login(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get('/delete/1')
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    assert b"Please log in" in rv.data
+
 
 def test_search_message_title(client):
     """Ensure that user can search by title"""
