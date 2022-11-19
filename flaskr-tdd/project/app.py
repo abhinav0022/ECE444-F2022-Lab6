a3402328a55edff1c135f391edb2c4411e923c24
Abhinav Sanjeeva Prasad <abhinavsanjeevaprasad@Abhinavs-MacBook-Pro.local>
2022-11-19 12:20:02 -0500

    Finished Postgres Heroku.

--- flaskr-tdd/project/app.py
+++ flaskr-tdd/project/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 from pathlib import Path
 from functools import wraps
 from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort, jsonify
@@ -6,13 +7,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 basedir = Path(__file__).resolve().parent
+DATABASE = "flaskr.db"
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
 
 # configuration
-DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+SQLALCHEMY_DATABASE_URI = url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
